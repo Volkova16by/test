4b46336ec34d9348f95afdb1762876953a38f411
Volkova16by <153448928+Volkova16by@users.noreply.github.com>
2024-02-01 19:45:47 +0300

    Добавили задачи

---
+++ Sem7/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        InverseArray(array, array.Length - 1);
+    }
+
+    static void InverseArray(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            InverseArray(array, index - 1);
+        }
+    }
+
+}
